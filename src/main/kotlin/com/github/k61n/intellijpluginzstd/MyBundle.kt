204510c936806774b2ebd03a2a927ee591bc4c4c
GitHub Action <action@github.com>
2025-10-13 09:57:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/k61n/intellijpluginzstd/MyBundle.kt
+++ src/main/kotlin/com/github/k61n/intellijpluginzstd/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.k61n.intellijpluginzstd
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/k61n/intellijpluginzstd/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/k61n/intellijpluginzstd/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.k61n.intellijpluginzstd.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
