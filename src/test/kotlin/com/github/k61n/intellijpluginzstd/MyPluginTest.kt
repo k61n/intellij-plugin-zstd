9598467956105594e2883e00d825fd08d41598ad
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2025-10-14 12:56:59 +0200

    test: remove unnecessary tests

--- src/test/kotlin/com/github/k61n/intellijpluginzstd/MyPluginTest.kt
+++ src/test/kotlin/com/github/k61n/intellijpluginzstd/MyPluginTest.kt
@@ -1,10 +1,7 @@
 package com.github.k61n.intellijpluginzstd
 
-import com.intellij.ide.highlighter.XmlFileType
-import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
-import com.intellij.util.PsiErrorElementUtil
 import com.github.k61n.intellijpluginzstd.services.MyProjectService
 import com.intellij.openapi.components.service
 import kotlin.io.path.Path
@@ -14,24 +11,6 @@ import kotlin.io.path.writeText
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
-
-    fun testXMLFile() {
-        val psiFile = myFixture.configureByText(XmlFileType.INSTANCE, "<foo>bar</foo>")
-        val xmlFile = assertInstanceOf(psiFile, XmlFile::class.java)
-
-        assertFalse(PsiErrorElementUtil.hasErrors(project, xmlFile.virtualFile))
-
-        assertNotNull(xmlFile.rootTag)
-
-        xmlFile.rootTag?.let {
-            assertEquals("foo", it.name)
-            assertEquals("bar", it.value.text)
-        }
-    }
-
-    fun testRename() {
-        myFixture.testRename("foo.xml", "foo_after.xml", "a2")
-    }
 
     fun testZstdCompressionAndDecompression() {
         val service = project.service<MyProjectService>()
@@ -50,6 +29,4 @@ class MyPluginTest : BasePlatformTestCase() {
         val decompressedContent = Path(decompressedFilename).readText(Charsets.UTF_8)
         assertEquals(testContent, decompressedContent)
     }
-
-    override fun getTestDataPath() = "src/test/testData/rename"
 }
