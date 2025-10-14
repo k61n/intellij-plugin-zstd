aef05b1c81ae9671387dbcdef8828b4d1877cff7
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2025-10-14 08:56:29 +0200

    add label intercepting currently opened filename

--- src/main/kotlin/com/github/k61n/intellijpluginzstd/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/k61n/intellijpluginzstd/toolWindow/MyToolWindowFactory.kt
@@ -1,45 +1,54 @@
 package com.github.k61n.intellijpluginzstd.toolWindow
 
+import com.github.k61n.intellijpluginzstd.MyBundle
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.service
-import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.fileEditor.FileEditorManagerEvent
+import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
-import com.intellij.ui.components.JBPanel
+import com.intellij.ui.components.panels.VerticalBox
 import com.intellij.ui.content.ContentFactory
-import com.github.k61n.intellijpluginzstd.MyBundle
 import com.github.k61n.intellijpluginzstd.services.MyProjectService
-import javax.swing.JButton
-
 
 class MyToolWindowFactory : ToolWindowFactory {
-
-    init {
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
-    }
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val myToolWindow = MyToolWindow(toolWindow)
         val content = ContentFactory.getInstance().createContent(myToolWindow.getContent(), null, false)
+        content.setDisposer(myToolWindow)
         toolWindow.contentManager.addContent(content)
     }
 
     override fun shouldBeAvailable(project: Project) = true
 
-    class MyToolWindow(toolWindow: ToolWindow) {
+    class MyToolWindow(private val toolWindow: ToolWindow) : Disposable {
 
-        private val service = toolWindow.project.service<MyProjectService>()
+        private val project = toolWindow.project
+        private val service = project.service<MyProjectService>()
 
-        fun getContent() = JBPanel<JBPanel<*>>().apply {
-            val label = JBLabel(MyBundle.message("randomLabel", "?"))
+        private val labelCurrentFile = JBLabel("No file")
 
-            add(label)
-            add(JButton(MyBundle.message("shuffle")).apply {
-                addActionListener {
-                    label.text = MyBundle.message("randomLabel", service.getRandomNumber())
+        fun getContent() = VerticalBox().apply {
+            add(labelCurrentFile)
+            project.messageBus.connect(this@MyToolWindow).subscribe(
+                FileEditorManagerListener.FILE_EDITOR_MANAGER,
+                object : FileEditorManagerListener {
+                    override fun selectionChanged(event: FileEditorManagerEvent) {
+                        labelCurrentFile.text = MyBundle.message("currentfile", event.newFile?.presentableUrl ?: "No file")
+                    }
                 }
-            })
+            )
+//            add(JButton("Compress").apply {
+//                addActionListener {
+//                    ...
+//                }
+//            })
+        }
+
+        override fun dispose() {
         }
     }
 }
